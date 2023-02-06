517273fb5fba31fa85c53ceb1081d2a91c64d285
AdminPin <1>
2023-02-07 00:59:32 +0300

    Брат - Создал меню поиска

--- app/src/main/java/com/pinkin/meetingprotocol/Fragments/MainFragment.kt
+++ app/src/main/java/com/pinkin/meetingprotocol/Fragments/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.pinkin.meetingprotocol.R
 import com.pinkin.meetingprotocol.databinding.FragmentMainBinding
@@ -27,11 +28,26 @@ class MainFragment : Fragment() {
             navigator().showAddProtocol()
         }
 
-        binding.toolbar.inflateMenu(R.menu.main_toolbar)
-        binding.toolbar.setTitle(R.string.app_name)
+        binding.toolbar.apply {
+
+            inflateMenu(R.menu.main_toolbar)
+            setTitle(R.string.app_name)
+
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.searchOption_all,R.id.searchOption_mans,R.id.searchOption_protocol -> {
+                        it.setChecked(true)
+                        Toast.makeText(context, "${it.title} pressed", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+
+
 
         return binding.root
     }
-
 
 }
