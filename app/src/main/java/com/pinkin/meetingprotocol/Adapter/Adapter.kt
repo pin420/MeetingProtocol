7e62c76be716553d651d3af24dd981ee6d55a516
Pin <Pin>
2023-02-09 22:17:15 +0300

    Создан базовый RecyclerView

---
+++ app/src/main/java/com/pinkin/meetingprotocol/Adapter/Adapter.kt
@@ -0,0 +1,43 @@
+package com.pinkin.meetingprotocol.Adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pinkin.datasource.Room.Entities.ProtocolDbEntity
+import com.pinkin.meetingprotocol.databinding.ItemMeetBinding
+
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+
+    var protocols: List<ProtocolDbEntity> = emptyList()
+        @SuppressLint("NotifyDataSetChanged")
+        set(newValue) {
+            field = newValue
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount(): Int = protocols.size
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMeetBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val protocol = protocols[position]
+
+        holder.binding.apply {
+            meetTitle.text = protocol.name
+            meetDate.text = protocol.date
+            meetTime.text = protocol.time
+        }
+
+    }
+
+
+    class ViewHolder(val binding: ItemMeetBinding) : RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/pinkin/meetingprotocol/Fragments/MainFragment.kt
+++ app/src/main/java/com/pinkin/meetingprotocol/Fragments/MainFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pinkin.datasource.Room.Entities.ProtocolDbEntity
+import com.pinkin.meetingprotocol.Adapter.Adapter
 import com.pinkin.meetingprotocol.R
 import com.pinkin.meetingprotocol.databinding.FragmentMainBinding
 import com.pinkin.meetingprotocol.navigator
@@ -21,6 +25,55 @@ class MainFragment : Fragment() {
     ): View? {
 
         val binding = FragmentMainBinding.inflate(inflater, container, false)
+
+
+        val adapterProtocols = Adapter()
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        binding.meetsRecyclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = adapterProtocols
+        }
+
+        // ТЕСТОВЫЙ КОД
+        val list: List<ProtocolDbEntity> = listOf(
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Roman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"grege", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rosggsegman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"Rsegsergoman", "09.02.2023", "22:13", ""),
+            ProtocolDbEntity(1,"esagagre", "09.02.2023", "22:13", ""),
+            )
+        adapterProtocols.protocols = list
+        // ТЕСТОВЫЙ КОД
+
+
+
+
+
 
 
         binding.addProtocol.setOnClickListener {
